093b2202b4b1c1383b0abaa13e1764adc92f7379
JavierHuertas <jhuertasozuna@gmail.com>
2017-07-13 09:49:54 -0600

    Segunda clase - 2

--- services.js
+++ services.js
@@ -2,34 +2,42 @@ var jwt = require('jsonwebtoken');
 var services = {};
 services.verificar = function(req, res, next) {
 	console.log("Funcion para verificar el token");
-	var header = req.headers["Authorization"];
-	if(typeof header !== undefined) {
-		console.log("Si trae la cabecera de Authorization");
-		var arrayHeader = header.split(" ");
-		var token = arrayHeader[1];
-		if(token) {
-			console.log("Si existe el token");
-			jwt.verify(token, 'shh', function(err, decoded) {
+	var header = req.headers.authorization;
+	console.log(req.headers);
+    if (typeof header !== 'undefined') {
+        var headerArray = header.split(" ");
+        var token = headerArray[1];
+        if(token) {
+        	console.log("Si existe el token");
+        	jwt.verify(token, 'shh', function(err, decoded){
 				if(err) {
 					if(err.name == "TokenExpiredError") {
-						console.log("El token ya expiro");
-						res.json({
-							estado: false,
-							mensaje: "Autenticacion fallida, expiro el token"
-						});
+						console.log("El token ya expiro");	
 					}
+					return res.json({
+						success: false,
+						mensaje: 'Autenticacion fallida, expiró el token'
+					});
 				} else {
-					console.log("El token ya fue verificado");
+					console.log("token decodificado");
 					req.token = token;
 					next();
 				}
 			});
-		} else {
-			console.log("No existe el token");
-		}
-	} else {
-		console.log("No trae la cabecera de Authorization");
-		next();
-	}
+        } else {
+        	console.log("No existe el token");
+        	res.json({
+	        	estado: false,
+	        	mensaje: "No existe el token"
+	        });
+        }
+    } else {
+        console.log("No lleva la cabezera authorization");
+        res.json({
+        	estado: false,
+        	mensaje: "No lleva la cabezera authorization"
+        });
+    }	
 }
+
 module.exports = services;
